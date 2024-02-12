ef6e1316f0b03b3263bb88b429bd00e58e75b519
Sharif Hasani <sharif.bashoma@gmail.com>
2024-02-12 15:57:24 +0330

    fix load building dataset

--- vehiclereid/datasets/building.py
+++ vehiclereid/datasets/building.py
@@ -25,19 +25,22 @@ class Building(BaseImageDataset):
     def __init__(self, root='datasets', verbose=True, **kwargs):
         super(Building, self).__init__(root)
         self.dataset_dir = osp.join(self.root, self.dataset_dir)
-        self.train_dir = osp.join(self.dataset_dir, 'image_train')
+        # self.train_dir = osp.join(self.dataset_dir, 'image_train')
+        self.train_dir = self.dataset_dir
         # self.train_list = None
-        self.train_list = osp.join(self.dataset_dir, 'name_train.txt')
-        self.query_dir = osp.join(self.dataset_dir, 'image_query')
-        self.query_list = osp.join(self.dataset_dir, 'name_query.txt')
-        self.gallery_dir = osp.join(self.dataset_dir, 'image_test')
-        self.gallery_list = osp.join(self.dataset_dir, 'name_test.txt')
+        # self.train_list = osp.join(self.dataset_dir, 'name_train.txt')
+        # self.query_dir = osp.join(self.dataset_dir, 'image_query')
+        self.query_dir = self.dataset_dir
+        # self.query_list = osp.join(self.dataset_dir, 'name_query.txt')
+        # self.gallery_dir = osp.join(self.dataset_dir, 'image_test')
+        self.gallery_dir = self.dataset_dir
+        # self.gallery_list = osp.join(self.dataset_dir, 'name_test.txt')
 
         self.check_before_run()
 
-        train = self.process_dir(self.train_dir, self.train_list, relabel=True)
-        query = self.process_dir(self.query_dir, self.query_list, relabel=False)
-        gallery = self.process_dir(self.gallery_dir, self.gallery_list, relabel=False)
+        train = self.process_dir(self.train_dir)
+        query = self.process_dir(self.query_dir)
+        gallery = self.process_dir(self.gallery_dir)
 
         if verbose:
             print('=> Building loaded')
@@ -63,18 +66,14 @@ class Building(BaseImageDataset):
             raise RuntimeError('"{}" is not available'.format(self.gallery_dir))
 
     def process_dir(self, dir_path, list_path=None, relabel=False):
-        if list_path is None:
-            img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
-        else:
-            name_list = open(list_path).readlines()
-            img_paths = []
-            for index in range(len(name_list)):
-                img_paths.append(osp.join(dir_path, name_list[index].strip()))
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+
         pattern = re.compile(r"[/\w|:|\s|\\|-]*(\d?\d?\d)_(sat|side)_(\d?\d?).jpg$")
 
         pid_container = set()
         for img_path in img_paths:
-            image_number, category, cam_id = map(int, pattern.search(img_path).groups())
+            pid, category, camid = pattern.search(img_path).groups()
+            pid = int(pid)
             if pid == -1:
                 continue  # junk images are just ignored
             pid_container.add(pid)
@@ -82,14 +81,15 @@ class Building(BaseImageDataset):
 
         dataset = []
         for img_path in img_paths:
-            pid, camid = map(int, pattern.search(img_path).groups())
+            pid, category, camid = pattern.search(img_path).groups()
+            pid, camid = int(pid), int(camid)
             if pid == -1:
                 continue  # junk images are just ignored
-            assert 0 <= pid <= 1501  # pid == 0 means background
-            assert 1 <= camid <= 20
-            camid -= 1  # index starts from 0
-            if relabel:
-                pid = pid2label[pid]
+            # assert 0 <= pid <= 1501  # pid == 0 means background
+            # assert 1 <= camid <= 20
+            # camid -= 1  # index starts from 0
+            # if relabel:
+            #     pid = pid2label[pid]
             dataset.append((img_path, pid, camid))
 
         return dataset
