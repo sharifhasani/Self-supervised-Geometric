70062898189f961c4c4fe1456a6f0e9b6abebb4d
Sharif Hasani <sharif.bashoma@gmail.com>
2024-05-09 14:20:31 +0330

    fix dataloader

--- vehiclereid/datasets/building.py
+++ vehiclereid/datasets/building.py
@@ -53,7 +53,6 @@ class Building(BaseImageDataset):
         self.num_train_pids, self.num_train_imgs, self.num_train_cams = self.get_imagedata_info(self.train)
         self.num_query_pids, self.num_query_imgs, self.num_query_cams = self.get_imagedata_info(self.query)
         self.num_gallery_pids, self.num_gallery_imgs, self.num_gallery_cams = self.get_imagedata_info(self.gallery)
-        exit()
 
     def check_before_run(self):
         """Check if all files are available before going deeper"""
